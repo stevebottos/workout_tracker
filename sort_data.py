740298e5a75976fce8df2d3d8ea60e158abf52ad
stevebottos <steve.bottos@hotmail.ca>
2020-03-25 17:49:08 -0700

    adding some initial code

--- sort_data.py
+++ sort_data.py
@@ -1,4 +1,7 @@
 import cv2
 import os
 
-cv2.waitKey()
+input_folder = "unsorted_images"
+out_class1 = "train/class1"  # This class represents the "top" position of a pushup
+out_class2 = "train/class2"  # This class represents the "bottom" position of a pushup
+out_class2 = "train/class3"  # This class represents anything that doesn't fit in the other two categories
