2c42f7da08011fbc3afcac9a9a44f59164d661cb
j.lemoine@qimia.io <j.lemoine@qimia.io>
2020-03-31 18:46:42 -0700

    Update .gitignore

--- convert_mov_to_mp4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Usage: ./convert_mov_to_mp4.sh [directory_name_with_movs]
-
-for dir in "$@"
-do
-   ffmpeg -i $dir -vcodec copy -acodec copy $(echo $dir | cut -d "." -f 1).mp4
-done
